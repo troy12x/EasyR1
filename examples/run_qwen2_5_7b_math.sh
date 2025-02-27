04cb5206c5c25cd3689a04abf63b0b279f5f35c9
Eiad Gomaa <61633360+troy12x@users.noreply.github.com>
2025-02-27 23:53:45 +0200

    Update run_qwen2_5_7b_math.sh

--- examples/run_qwen2_5_7b_math.sh
+++ examples/run_qwen2_5_7b_math.sh
@@ -2,7 +2,7 @@ set -x
 
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
-MODEL_PATH=Qwen/Qwen2.5-7B-Instruct  # replace it with your local file path
+MODEL_PATH=silx-ai/Quasar-2.5-7B-Ultra  # replace it with your local file path
 
 python3 -m verl.trainer.main \
     config=examples/grpo_example.yaml \
